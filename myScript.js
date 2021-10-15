03dee1f97c9ebe48237719558014eae82cc94d5f
romhappy <63718118+romhappy@users.noreply.github.com>
2021-10-15 21:31:07 +0200

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,18 @@
+var pictoBoire = "media/boire.png";
+var pictoManger = "media/manger.png";
+let count = 0;
+let result;
+
+function createimg(key,sizeA,sizeB) {
+var theimage = document.createElement("img");
+theimage.setAttribute('src', key);
+theimage.setAttribute('alt', 'image');
+theimage.height = sizeA;
+theimage.width = sizeB;
+if (count<5) {
+    document.getElementById('sentence_container').appendChild(theimage); 
+   } else {
+       alert("effacez les pictos")
+   }
+count = count + 1;
+}
