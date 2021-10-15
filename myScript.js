1b6546953d87d1969fec2bf69b5ca500f744990b
romhappy <rom100784@gmail.com>
2021-10-15 23:12:19 +0200

    sentence container et button delete

--- myScript.js
+++ myScript.js
@@ -1,7 +1,6 @@
-var pictoBoire = "media/boire.png";
-var pictoManger = "media/manger.png";
+let pictoBoire = "media/boire.png";
+let pictoManger = "media/manger.png";
 let count = 0;
-let result;
 
 function createimg(key,sizeA,sizeB) {
 var theimage = document.createElement("img");
@@ -10,7 +9,7 @@ theimage.setAttribute('alt', 'image');
 theimage.height = sizeA;
 theimage.width = sizeB;
 if (count<5) {
-    document.getElementById('sentence_container').append(theimage); 
+    document.getElementById('sentence_container').appendChild(theimage); 
    } else {
        alert("effacez les pictos")
    }
